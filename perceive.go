b960e94fd96a21db5234801962f89ee99b86d310
unknown <calebhwin@gmail.com>
2018-09-16 22:58:41 -0700

    support for custom activation functions

--- perceive.go
+++ perceive.go
@@ -1,25 +1,83 @@
 package perceive
 
 import (
+  "math"
 )
 
-func heaviside(x float32) float32 {
+type ActivationFunction func(x float64) (result float64)
+
+func Heaviside(x float64) float64 {
   if x < 0 {
     return 0
   }
   return 1
 }
 
-type Parameters struct {
-  initialWeights []float32
-  initialBias float32
-  learningRate float32
+func Linear(x float64) float64 {
+  return x
 }
 
-func Perceive(trainingInputs [][]float32, trainingOutputs []float32, inputs [][]float32, parameters Parameters) []float32 {
+func Logistic(x float64) float64 {
+  return 1 / (1 + math.Pow(math.E, -x))
+}
+
+func TanH(x float64) float64 {
+  return 2 * Logistic(2 * x) - 1
+}
+
+func Rectifier(x float64) float64 {
+  return math.Max(0.0, x)
+}
+
+func Bipolar(x float64) float64 {
+  if x < 0 {
+    return -1
+  } else if x > 0 {
+    return 1
+  }
+  return 0
+}
+
+func BipolarSigmoid(x float64) float64 {
+  return (1 - math.Pow(math.E, -x)) / (1 + math.Pow(math.E, -x))
+}
+
+func LeCunTahH(x float64) float64 {
+  return 1.7159 * TanH(2 * x / 3)
+}
+
+func HardTahH(x float64) float64 {
+  return math.Max(-1, math.Min(1, x))
+}
+
+func Abs(x float64) float64 {
+  return math.Abs(x)
+}
+
+func SmoothRectifier(x float64) float64 {
+  return math.Log(1 + math.Pow(math.E, x))
+}
+
+func Logit(x float64) float64 {
+  return math.Log(x / (1 - x))
+}
+
+func Gaussian(x float64) float64 {
+  return math.Pow(math.E, -0.5 * math.Pow(x, 2))
+}
+
+type Parameters struct {
+  initialWeights []float64
+  initialBias float64
+  learningRate float64
+  activationFunction ActivationFunction
+}
+
+func Perceive(trainingInputs [][]float64, trainingOutputs []float64, inputs [][]float64, parameters Parameters) []float64 {
   weights := parameters.initialWeights
   bias := parameters.initialBias
   learningRate := parameters.learningRate
+  activationFunction := parameters.activationFunction
 
   // adjust weights and random values with training data
   for i, trainingInput := range trainingInputs {
@@ -31,7 +89,7 @@ func Perceive(trainingInputs [][]float32, trainingOutputs []float32, inputs [][]
     for i, input := range trainingInput {
       actualOuput += input * weights[i]
     }
-    actualOuput = float32(heaviside(actualOuput))
+    actualOuput = float64(activationFunction(actualOuput))
 
     // calculate cost
     cost := expectedOutput - actualOuput
@@ -45,7 +103,7 @@ func Perceive(trainingInputs [][]float32, trainingOutputs []float32, inputs [][]
     bias += cost * learningRate
   }
 
-  outputs := []float32{}
+  outputs := []float64{}
 
   for _, inputInputs := range inputs {
     // calculate sum of weighted inputs
@@ -55,7 +113,7 @@ func Perceive(trainingInputs [][]float32, trainingOutputs []float32, inputs [][]
     }
 
     // calculate output by applying activation function
-    output := heaviside(inputSum)
+    output := activationFunction(inputSum)
 
     // add output to outputs array
     outputs = append(outputs, output)
--- perceive_test.go
+++ perceive_test.go
@@ -6,15 +6,15 @@ import (
 )
 
 func TestBasic(t *testing.T) {
-  trainingInputs := [][]float32{
-    []float32{-0.8, -0.7, -0.5,},
-    []float32{-0.5, -0.4, -0.4,},
-    []float32{0.0, 0.2, 0.5,},
-    []float32{-0.5, -0.5, -0.4,},
-    []float32{0.7, 0.8, 0.9,},
+  trainingInputs := [][]float64{
+    []float64{-0.8, -0.7, -0.5,},
+    []float64{-0.5, -0.4, -0.4,},
+    []float64{0.0, 0.2, 0.5,},
+    []float64{-0.5, -0.5, -0.4,},
+    []float64{0.7, 0.8, 0.9,},
   }
 
-  trainingOutputs := []float32{
+  trainingOutputs := []float64{
     0,
     0,
     1,
@@ -22,16 +22,17 @@ func TestBasic(t *testing.T) {
     1,
   }
 
-  myInputs := [][]float32{
-    []float32{-0.8, -0.7, -0.5,},
-    []float32{-0.5, -0.4, -0.4,},
-    []float32{0.0, 0.2, 0.5,},
+  myInputs := [][]float64{
+    []float64{-0.8, -0.7, -0.5,},
+    []float64{-0.5, -0.4, -0.4,},
+    []float64{0.0, 0.2, 0.5,},
   }
 
   myOutputs := Perceive(trainingInputs, trainingOutputs, myInputs, Parameters{
-    []float32{0, 0, 0},
+    []float64{0, 0, 0},
     0,
     0.1,
+    Heaviside,
   })
 
   for _, myOutput := range myOutputs {
