58242f6ce7150b528782439ffdaf0ad96c0211e0
Caleb Winston <calebhwin@gmail.com>
2018-10-08 12:25:29 -0700

    simplified API

--- perceive.go
+++ perceive.go
@@ -4,18 +4,34 @@ package perceive
 
 type Parameters struct {
   initialWeights []float64
-  initialBias float64
   learningRate float64
   numEpochs int
   //activationFunction ActivationFunction
 }
 
-func Perceive(trainingInputs [][]float64, trainingOutputs []float64, inputs [][]float64, parameters Parameters) []float64 {
+func Perceive(trainingInputsClass1 [][]float64, trainingInputsClass2 [][]float64, inputs [][]float64, parameters Parameters) ([][]float64, [][]float64) {
   weights := parameters.initialWeights
-  bias := parameters.initialBias
+  bias := 0.0
   learningRate := parameters.learningRate
   numEpochs := parameters.numEpochs
   activationFunction := Heaviside//parameters.activationFunction
+
+  // generate training data from 2 classes of input data
+  trainingInputs := make([][]float64, len(trainingInputsClass1) + len(trainingInputsClass2))
+  trainingOutputs := make([]float64, len(trainingInputsClass1) + len(trainingInputsClass2))
+  nextTrainingInputIndex := 0
+  for _, trainingInput := range trainingInputsClass1 {
+    trainingInputs[nextTrainingInputIndex] = []float64{}
+    trainingInputs[nextTrainingInputIndex] = append(trainingInputs[nextTrainingInputIndex], trainingInput...)
+    trainingOutputs = append(trainingOutputs, 0.0)
+    nextTrainingInputIndex++
+  }
+  for _, trainingInput := range trainingInputsClass2 {
+    trainingInputs[nextTrainingInputIndex] = []float64{}
+    trainingInputs[nextTrainingInputIndex] = append(trainingInputs[nextTrainingInputIndex], trainingInput...)
+    trainingOutputs = append(trainingOutputs, 1.0)
+    nextTrainingInputIndex++
+  }
 
   for numEpochs > 0 {
     // adjust weights and random values with training data
@@ -24,19 +40,19 @@ func Perceive(trainingInputs [][]float64, trainingOutputs []float64, inputs [][]
       expectedOutput := trainingOutputs[i]
 
       // calculate actual output of training input
-      actualOuput := 0.0
-      actualOuput += bias
-      for i, input := range trainingInput {
-        actualOuput += input * weights[i]
+      actualOutput := 0.0
+      actualOutput += bias
+      for j, input := range trainingInput {
+        actualOutput += input * weights[j]
       }
-      actualOuput = activationFunction(actualOuput)
+      actualOutput = activationFunction(actualOutput)
 
       // calculate cost
-      cost := expectedOutput - actualOuput
+      cost := expectedOutput - actualOutput
 
       // adjust weights based on cost
-      for i, input := range trainingInput {
-        weights[i] += input * cost * learningRate
+      for j, input := range trainingInput {
+        weights[j] += input * cost * learningRate
       }
 
       // adjust bias based on cost
@@ -46,7 +62,8 @@ func Perceive(trainingInputs [][]float64, trainingOutputs []float64, inputs [][]
     numEpochs--
   }
 
-  outputs := []float64{}
+  outputs := [][]float64{}
+  outputClassIndices := []float64{}
 
   for _, inputInputs := range inputs {
     // calculate sum of weighted inputs
@@ -55,13 +72,31 @@ func Perceive(trainingInputs [][]float64, trainingOutputs []float64, inputs [][]
       inputSum += input * weights[i]
     }
 
-    // calculate output by applying activation function
-    output := activationFunction(inputSum)
+    // calculate output class index by applying activation function
+    outputClassIndex := activationFunction(inputSum)
+
+    // add output class index to output class indices array
+    outputClassIndices = append(outputClassIndices, outputClassIndex)
 
     // add output to outputs array
-    outputs = append(outputs, output)
+    outputs = append(outputs, []float64{})
+    outputs[len(outputs) - 1] = append(outputs[len(outputs) - 1], inputInputs...)
+  }
+
+  // generated seperated classes of outputs
+  outputsClass1 := [][]float64{}
+  outputsClass2 := [][]float64{}
+  for i, outputClassIndex := range outputClassIndices {
+    if int(outputClassIndex) == 0 {
+      outputsClass1 = append(outputsClass1, []float64{})
+      outputsClass1[len(outputsClass1) - 1] = append(outputsClass1[len(outputsClass1) - 1], outputs[i]...)
+    }
+    if int(outputClassIndex) == 1 {
+      outputsClass2 = append(outputsClass2, []float64{})
+      outputsClass2[len(outputsClass2) - 1] = append(outputsClass2[len(outputsClass2) - 1], outputs[i]...)
+    }
   }
 
   // return sum of weighted inputs passed into activation function
-  return outputs
+  return outputsClass1, outputsClass2
 }
--- perceive_test.go
+++ perceive_test.go
@@ -6,36 +6,36 @@ import (
 )
 
 func TestBasic(t *testing.T) {
-  trainingInputs := [][]float64{
+  trainingInputsClass1 := [][]float64{
     []float64{-0.8, -0.7, -0.5,},
     []float64{-0.5, -0.4, -0.4,},
-    []float64{0.0, 0.2, 0.5,},
     []float64{-0.5, -0.5, -0.4,},
-    []float64{0.7, 0.8, 0.9,},
   }
 
-  trainingOutputs := []float64{
-    0,
-    0,
-    1,
-    0,
-    1,
+  trainingInputsClass2 := [][]float64{
+    []float64{0.0, 0.2, 0.5,},
+    []float64{0.7, 0.8, 0.9,},
   }
 
   myInputs := [][]float64{
     []float64{-0.8, -0.7, -0.5,},
     []float64{-0.5, -0.4, -0.4,},
-    []float64{0.0, 0.2, 0.5,},
+    []float64{1.7, 1.8, 1.9,},
   }
 
-  myOutputs := Perceive(trainingInputs, trainingOutputs, myInputs, Parameters{
+  myOutputsClass1, myOutputsClass2 := Perceive(trainingInputsClass1, trainingInputsClass2, myInputs, Parameters{
     []float64{0, 0, 0},
-    0,
     0.1,
     3,
   })
 
-  for _, myOutput := range myOutputs {
+  fmt.Println("Class 1:")
+  for _, myOutput := range myOutputsClass1 {
+    fmt.Println(myOutput)
+  }
+
+  fmt.Println("Class 2:")
+  for _, myOutput := range myOutputsClass2 {
     fmt.Println(myOutput)
   }
 }
